d6e77eada36f1c25341d145f3ff245878dbc8b48
Christopher Justice <justcd@mail.broward.edu>
2017-06-12 20:14:15 -0400

    add play button

--- video player/scripts.js
+++ video player/scripts.js
@@ -3,6 +3,16 @@ const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
 const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
-const skipButtons = player.querySelector("[data-skip]");
-const ranges = player.querySelector(".player__slider");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+function togglePlay() {
+	if(video.paused) {
+		video.play();
+	} else {
+		video.pause();
+	}
+}
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
 
